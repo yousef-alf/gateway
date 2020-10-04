d297025a3b0a3597401faa770a851c6dfb876fe6
yousef-alf <61252354+yousef-alf@users.noreply.github.com>
2020-10-04 04:33:59 +0300

    Create station2.py

---
+++ station2.py
@@ -0,0 +1,26 @@
+import socket
+import random
+import time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = '127.0.0.1'
+
+port = 5555
+
+s.connect((host, port))
+i = 0;
+timeout = time.time() + 0.1
+while True:
+
+    while time.time()<=timeout:
+
+
+        y = random.randint(0,10)
+
+        s.send((str(y).encode('utf-8')))
+
+    break;
+
+
+s.close()
